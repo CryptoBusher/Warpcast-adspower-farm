ddc9afaec11f69b0e67d6feb1619b140e749a383
Crypto B <103379312+CryptoBusher@users.noreply.github.com>
2024-05-29 14:48:28 +0300

    Update farm_warpcast.py

--- farm_warpcast.py
+++ farm_warpcast.py
@@ -9,53 +9,74 @@ from src.WarpcastProfile import WarpcastProfile
 from data.profile_ids import profile_ids
 from data.config import config
 
+
 logger.remove()
 logger_level = "DEBUG" if config['show_debug_logs'] else "INFO"
-logger.add(stderr, level=logger_level, format="<white>{time:HH:mm:ss}</white> | <level>{level: <8}</level> | <white>{"
-                                              "message}</white>")
+log_format = "<white>{time:HH:mm:ss}</white> | <level>{level: <8}</level> | <white>{message}</white>"
+logger.add(stderr, level=logger_level, format=log_format)
+logger.add("data/debug_log.log", level="DEBUG", format=log_format)
+
 
 print(busher_logo)
 print(social_links)
 
+
 def start_farm(_account: WarpcastProfile):
     actions_list = {
         _account.cast_on_homepage: {
+            "use_module": config['module_switches']['cast_on_homepage'],
             "probability": config['cast_on_homepage']['use_module_probability'],
             "name": "cast on homepage"
         },
-        _account.subscribe_to_users: {
-            "probability": config['subscribe_via_explore']['to_users_probability'],
-            "name": "subscribe to users"
-        },
-        _account.subscribe_to_channels: {
-            "probability": config['subscribe_via_explore']['to_channels_probability'],
-            "name": "cast on channels"
-        },
         _account.surf_feed: {
+            "use_module": config['module_switches']['surf_feed'],
             "probability": config['surf_feed']['use_module_probability'],
             "name": "surf feed"
         },
+        _account.subscribe_to_users_via_explore: {
+            "use_module": config['module_switches']['subscribe_to_users_via_explore'],
+            "probability": config['subscribe_via_explore']['to_users_probability'],
+            "name": "subscribe to users via explore"
+        },
+        _account.subscribe_to_channels_via_explore: {
+            "use_module": config['module_switches']['subscribe_to_channels_via_explore'],
+            "probability": config['subscribe_via_explore']['to_channels_probability'],
+            "name": "subscribe to channels via explore"
+        },
         _account.subscribe_to_authors_via_search: {
+            "use_module": config['module_switches']['subscribe_to_authors_via_search'],
             "probability": config['subscribe_to_authors_via_search']['use_module_probability'],
             "name": "subscribe to authors via search"
         },
         _account.subscribe_to_channels_via_search: {
+            "use_module": config['module_switches']['subscribe_to_channels_via_search'],
             "probability": config['subscribe_to_channels_via_search']['use_module_probability'],
             "name": "subscribe to channels via search"
         },
         _account.subscribe_to_users_via_search: {
+            "use_module": config['module_switches']['subscribe_to_users_via_search'],
             "probability": config['subscribe_to_users_via_search']['use_module_probability'],
             "name": "subscribe to users via search"
         },
         _account.subscribe_to_mandatory_users: {
+            "use_module": config['module_switches']['subscribe_to_mandatory_users'],
             "probability": config['subscribe_to_mandatory_users']['use_module_probability'],
             "name": "subscribe to mandatory users"
         },
         _account.subscribe_to_mandatory_channels: {
+            "use_module": config['module_switches']['subscribe_to_mandatory_channels'],
             "probability": config['subscribe_to_mandatory_channels']['use_module_probability'],
             "name": "subscribe to mandatory channels"
         }
     }
+
+    switched_off_modules = []
+    for key, value in actions_list.items():
+        if not value["use_module"]:
+            switched_off_modules.append(key)
+
+    for key in switched_off_modules:
+        del actions_list[key]
 
     all_actions = list(actions_list.keys())
     shuffle(all_actions)
@@ -98,9 +119,9 @@ if __name__ == "__main__":
         logger.info(f"Amount of profiles to farm > total amount of profiles, adjusted")
         config["profiles_to_farm"] = len(warpcast_accounts)
 
-    # # for debugging
-    # self = warpcast_accounts[0]
-    # self.open_profile()
+    # for debugging
+    self = warpcast_accounts[0]
+    self.open_profile()
 
     for i in range(config["profiles_to_farm"]):
         account = warpcast_accounts.pop(randint(0, len(warpcast_accounts) - 1))
